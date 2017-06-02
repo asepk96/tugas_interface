4a15746d0879ec744775feb818de76fad8eafd4a
asepkrn <akurniawan848@gmail.com>
2017-06-02 21:26:04 +0700

    Add files via upload

---
+++ tugas.java
@@ -0,0 +1,29 @@
+/**
+*
+*@ aunthor Asep Kurniawan
+*/
+public interface Mahasiswa{
+	public String nama = "asep kurniawan";
+	public String nim = "14111015";
+}
+ /*interface takdapat di instalasi*/ 
+public static void main (String[] Args){
+	Reptil biawak = new Reptil();
+}
+
+*/class abstak dapat menginflementasikan interface dengan bebas*/
+public class biawak implements reptile {
+	public static void main (String[] Args){
+	protected String GetNama(){
+	return this.nama;
+	}
+	}
+}
+*/implemen variabel interface */
+public class biawak implemens reptil {
+	public static void main (String[] Args){
+	biawak cicak = new biawak();
+	cicak.nama;
+	cicak.jenis;
+	}
+ }
